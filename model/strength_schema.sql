fba2aa980ce3573a3bcf2daa7525950b70bd3ad5
Ryan Durk <rpdurk@gmail.com>
2020-11-03 18:05:59 -0800

    updated strengthApp database schema

---
+++ model/strength_schema.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS strengthApp_db;
+
+CREATE DATABASE strengthApp_db;
+
+USE strengthApp_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT NOT NULL,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    height INT NOT NULL,
+    weight INT NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE workouts (
+	id INT AUTO_INCREMENT NOT NULL,
+    workoutName VARCHAR(255) NOT NULL,
+    userId INT references users(id),
+    workoutDate DATE NOT NULL,
+    exercises VARCHAR(3000),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE exercises (
+	id INT AUTO_INCREMENT NOT NULL,
+    exerciseName VARCHAR(255) NOT NULL,
+    musclesUsed VARCHAR(255),
+    userId INT references users(id),
+    workoutName references workouts(id),
+    exerciseDate DATE NOT NULL,
+    setsAttempted INT,
+    reptitionsAttempted INT,
+    weightUsed INT,
+    timeUsed INT,
+    restUsed INT,
+    PRIMARY KEY (id)
+);
