7a7d544e6b6855702f650823cfcaec3ee6753a36
Muhammad Kamran <mkamran1967@gmail.com>
2020-11-03 16:57:27 -0800

    file structure

---
+++ model/exerciseQueries.js
@@ -0,0 +1,13 @@
+/* Exercises
+ *
+ *  ID INT Auto_INCREMENT NOT NULL,
+ *  Foreign Key from workout and user
+ *  NameOfExercise VARCHAR (50) Not NULL
+ *  Date INT (sql supported DateTime)
+ *  Sets INT NOT NULL,
+ *  Repetitions INT NOT NULL,
+ *  Weight INT
+ *  Time INT
+ *  Rest INT
+ *
+ */
---
+++ model/workoutQueries.js
@@ -0,0 +1,11 @@
+/**
+ *  Workout Queries
+ *
+ *  ID INT Auto_INCREMENT NOT NULL,
+ *  Foreign Key UserId of the User who made it
+ *  WorkoutName
+ *  Date INT NOT NULL,
+ *  Exercise choices VARCHAR(3000) NOT NULL, <- Will contain JSON String
+ *  Primary Key (id)
+ *
+ */
