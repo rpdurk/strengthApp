7460d6ffa8a5b19eac29b01cdb84edf1a73a9a47
huqian301 <huqian301@gmail.com>
2020-11-04 19:00:12 -0800

    adding SignUp page

--- client/src/pages/common/components/SignIn.js
+++ client/src/pages/common/components/SignIn.js
@@ -12,6 +12,7 @@ import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import SignUp  from './SignUp';
 
 function Copyright() {
   return (
@@ -110,7 +111,7 @@ export default function SignInSide() {
             </Button>
             <Grid container>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link component= {SignUp}  to='/SignUp' variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
