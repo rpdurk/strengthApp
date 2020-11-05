b0cd067abbbbf592de7156bac49c5cb7c81547cc
Muhammad Kamran <mkamran1967@gmail.com>
2020-11-04 22:09:01 -0800

    Fixed exporst for exerciseQueries, Added a Favicon.

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,13 @@ let connection;
 if (process.env.NODE_ENV === 'production') {
   connection = mysql.createConnection(process.env.JAWSDB_URL).promise();
 } else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'fwitter_db',
-  }).promise();
+  connection = mysql
+    .createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'strengthApp_db',
+    })
+    .promise();
 }
 module.exports = connection;
--- model/workoutQueries.js
+++ model/workoutQueries.js
@@ -1,25 +1,27 @@
 /**
  *  Workout Queries
  *
- *  ID INT Auto_INCREMENT NOT NULL,
- *  Foreign Key UserId of the User who made it
- *  WorkoutName
- *  Date INT NOT NULL,
- *  Exercise choices VARCHAR(3000) NOT NULL, <- Will contain JSON String
- *  Primary Key (id)
+ *	0 id INT AUTO_INCREMENT NOT NULL,
+ *  1 workoutName VARCHAR(255) NOT NULL,
+ *  2 userId INT references users(id),
+ *  3 workoutDate DATE NOT NULL,
+ *  4 exercises VARCHAR(255),
+ *    PRIMARY KEY (id)
  *
  */
 const findAllWorkouts = 'SELECT id, workoutName FROM workouts;';
-const findWorkoutByIdQuery = 'SELECT id, workooutName FROM workouts WHERE id = ?;';
+const findWorkoutByIdQuery =
+  'SELECT id, workooutName FROM workouts WHERE id = ?;';
 const findWorkoutByWorkoutName =
-    'SELECT id FROM workouts WHERE workoutName = ?;';
-const insertWorkout = 'INSERT INTO workout (workoutName, userId, workoutDate, exercsises) VALUES (?, ?, ?, ?);';
+  'SELECT id FROM workouts WHERE workoutName = ?;';
+const insertWorkout =
+  'INSERT INTO workout (workoutName, userId, workoutDate, exercsises) VALUES (?, ?, ?, ?);';
 const deleteWorkoutQuery = 'DELETE FROM workout WHERE ID = ?;';
 
 module.exports = {
-    findAllWorkouts,
-    findWorkoutByIdQuery,
-    findWorkoutByWorkoutName,
-    insertWorkout,
-    deleteWorkoutQuery,
+  findAllWorkouts,
+  findWorkoutByIdQuery,
+  findWorkoutByWorkoutName,
+  insertWorkout,
+  deleteWorkoutQuery,
 };
