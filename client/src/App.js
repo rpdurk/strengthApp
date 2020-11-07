870c3ecedca814fc7db770fb635cf2501ad458a7
bosshogg86 <jeffhogg86@gmail.com>
2020-11-06 16:54:05 -0800

    adds theme, dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
 import Drawer from "./pages/common/components/Drawer";
 import SignIn from "./pages/Signin";
 import SignUp from "./pages/SignUp";
@@ -8,29 +9,34 @@ import CreateWorkout from "./pages/CreateWorkout";
 import LogWorkout from "./pages/LogWorkout";
 import Progress from "./pages/Progress";
 import Account from "./pages/Account";
+import theme from "./utils/Theme";
 
 import { WrappedSignUp, WrappedSignIn } from "./pages/Viewer";
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/" component={SignIn} />
-      <Route path="/signup" component={SignUp} />
-      <Route path={["/dashboard", "/create", "/log", "/progress", "/account"]}>
-        <Drawer>
-          <Switch>
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/create" component={CreateWorkout} />
-            <Route path="/log" component={LogWorkout} />
-            <Route path="/progress" component={Progress} />
-            <Route path="/account" component={Account} />
-          </Switch>
-        </Drawer>
-      </Route>
-      {/* Manny's routes below */}
-      {/* <Route path="/signup" component={WrappedSignUp} />
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Route exact path="/" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route
+          path={["/dashboard", "/create", "/log", "/progress", "/account"]}
+        >
+          <Drawer>
+            <Switch>
+              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/create" component={CreateWorkout} />
+              <Route path="/log" component={LogWorkout} />
+              <Route path="/progress" component={Progress} />
+              <Route path="/account" component={Account} />
+            </Switch>
+          </Drawer>
+        </Route>
+        {/* Manny's routes below */}
+        {/* <Route path="/signup" component={WrappedSignUp} />
       <Route path="/signin" component={WrappedSignIn} /> */}
-    </Router>
+      </Router>
+    </ThemeProvider>
   );
 }
 
