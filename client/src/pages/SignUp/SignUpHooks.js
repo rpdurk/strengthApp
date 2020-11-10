e8500dd3aadf1a73f373485243cc237dfe28cd44
bosshogg86 <jeffhogg86@gmail.com>
2020-11-09 21:58:44 -0800

    sign up

--- client/src/pages/SignUp/SignUpHooks.js
+++ client/src/pages/SignUp/SignUpHooks.js
@@ -1,12 +1,18 @@
-import axios from 'axios';
-
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 // Create a user
+import { useDispatch } from "react-redux";
+import { setViewerToken } from "../Viewer";
 export const useCreateUser = () => {
-  const handleSaveUser = (formValues) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const handleSaveUser = formValues => {
     // console.log(formValues);
-    axios.post('/auth/signup', formValues).then((res) => {
-      console.log('response 👇');
-      console.log(res);
+    axios.post("/auth/signup", formValues).then(res => {
+      console.log("response 👇");
+      console.log(res.data);
+      dispatch(setViewerToken(res.data));
+      history.push("/dashboard");
     });
   };
 
