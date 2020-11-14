89ff3e83754c6e634378349c0fdc897a9eb99ebd
Ryan Durk <rpdurk@gmail.com>
2020-11-14 10:52:23 -0800

    updated routes

--- routes/apiRoutes/workoutRoutes.js
+++ routes/apiRoutes/workoutRoutes.js
@@ -9,9 +9,11 @@ const {
 
 // GET Routes
 // GET -> /:userId          -> Returns All workouts by User
-// GET -> /:height              -> Returns height by UserId
-// GET -> /:weightId     -> Returns weight by UserId
-// GET -> /:weightDate    -> Returns Dates of weight by UserId
+// GET -> /:workoutId     -> Returns all workouts by workout ID
+// GET -> /:workoutName    -> Returns all workout by a specific name
+// Post -> /addWorkout     -> Add workout object into the database
+// DELETE -> /deleteWorkout    -> Deletes workout object from the database
+
 
 // Get workouts by User ID
 router.get('/:userId', returnAllWorkoutsByUserId);
@@ -23,9 +25,9 @@ router.get('/:workoutId', returnWorkoutsById);
 router.get('/:workoutName', returnWorkoutsByWorkoutName);
 
 // Add a workout
-router.get('/addWorkout', addWorkout);
+router.post('/addWorkout', addWorkout);
 
 // Delete a workout
-router.get('/:workoutId', deleteWorkouts);
+router.delete('/:workoutId', deleteWorkouts);
 
 module.exports = router;
