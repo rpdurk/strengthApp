c11321c326c12c59f1e743aed48cf58c44e77a0b
Ryan Durk <rpdurk@gmail.com>
2020-11-16 16:33:28 -0800

    updated routes for heroku

--- index.js
+++ index.js
@@ -12,31 +12,31 @@ const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-  app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-  app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-  app.get('/create', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-  app.get('/log', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-  app.get('/progress', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-  app.get('/account', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-  app.get('*', (req, res) => {
-    console.log(`/*`);
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+  // app.get('/', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // });
+  // app.get('/signup', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // });
+  // app.get('/dashboard', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // });
+  // app.get('/create', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // });
+  // app.get('/log', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // });
+  // app.get('/progress', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // });
+  // app.get('/account', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // });
+  // app.get('*', (req, res) => {
+  //   console.log(`/*`);
+  //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // });
 }
 
 app.use(express.urlencoded({ extended: true }));
