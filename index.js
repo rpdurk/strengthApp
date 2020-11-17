72d2c76077b93f2bde29be475cf28bcff50095ad
Ryan Durk <rpdurk@gmail.com>
2020-11-16 16:02:56 -0800

    updated routes in index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  app.get('*', (req, res) => res.sendFile(path.resolve('client/build', 'index.html')));
 }
 
 app.use(express.urlencoded({ extended: true }));
